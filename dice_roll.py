4d62454e7b911fd179f629527da44562bc6e2c83
KJ <jkelvisdev23@gmail.com>
2022-05-21 18:00:40 -0500

    Update dice_roll.py

--- dice_roll.py
+++ dice_roll.py
@@ -3,23 +3,23 @@ import time
 
 answer = input('Do you want to roll the dice? Yes/no ')
 
-if answer.lower() == 'yes':
-    while 1 > 0:
+if answer.lower() == 'yes' or answer.lower() == 'y':
+    count = 0
+    while True:
         dice = range(1,7)
-        print('We are shaking the dice.')
-        time.sleep(1)
-        print('We are shaking the dice..')
-        time.sleep(1)
-        print('We are shaking the dice...')
-        time.sleep(1)
+        while count < 3:
+            time.sleep(1)
+            print('We are shaking the dice.')
+            count = count + 1
         print('dice was thrown')
         time.sleep(1)
         print('the dice is showing the number', random.choice(dice))
         again = input('Do you want to roll the dice again? Yes/No ')
-        if again.lower() == 'no':
+        count = 0
+        if again.lower() == 'no' or again.lower() == 'n':
             print('The game is closed')
             break
-elif answer.lower() == 'no':
+elif answer.lower() == 'no' or answer.lower() == 'n':
     print('The game will be closed.')
 else: 
     print('You should select a valid option')
